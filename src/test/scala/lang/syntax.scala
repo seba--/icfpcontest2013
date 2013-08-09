6cb64acae31bf2e76af596aac916092f168d4b1c
Sebastian Erdweg <seba.github@gmail.com>
2013-08-09 14:05:54 +0200

    syntax tests

---
+++ src/test/scala/lang/syntax.scala
@@ -0,0 +1,27 @@
+
+import org.scalatest.FunSuite
+import json.TrainingProblemStore
+
+class ParserSuite extends FunSuite {
+  
+  import lang.Abstract._
+  import lang.Concrete.parse
+  
+  test("id function") {
+    val res = parse("(lambda (x) x)")
+    assert(res === Left((Prg("x", Var("x")))))
+  }
+}
+
+class TrainingDataParserSuite extends FunSuite {
+
+  import lang.Concrete.parse
+  
+  test("training data") {
+    for (id <- TrainingProblemStore.ids) {
+      val prob = TrainingProblemStore.read(id)
+      val result = parse(prob.challenge)
+      assert(result.isLeft, result)
+    }
+  }
+}
