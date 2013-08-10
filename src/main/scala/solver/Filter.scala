40aa22d872d7b25fc548f813c6a61a0ce5a55864
Dennis Albrecht <Dennis.Albrecht.tud@gmx.de>
2013-08-10 22:33:58 +0200

    new max-function for FilterValues

--- src/main/scala/solver/Filter.scala
+++ src/main/scala/solver/Filter.scala
@@ -10,7 +10,7 @@ trait Filter {
   def init(spec: ProblemSpec): Unit
   // notify about new evaluation data, will be already registered in problem spec
   def notifyNewData(data: Map[Long, Long]): Unit
-  
+
   // keep all expressions that satisfy the predicate
   def filter(e: Exp): Int
 }
@@ -18,5 +18,11 @@ trait Filter {
 object FilterV {
   val OK = 0
   val STEP_INTO = 1
-  val STEP_OVER = 2  
+  val STEP_OVER = 2
+  def max(v1: Int, v2: Int): Int = {
+    if (v1 == STEP_OVER)
+      STEP_OVER
+    else
+      Math.max(v1, v2)
+  }
 }
