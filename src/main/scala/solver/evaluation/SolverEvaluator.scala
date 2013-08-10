d38ac0a69658c17556bf8aa6138ce2fd59b264b7
Joscha Drechsler <drechsler.joscha@gmail.com>
2013-08-10 18:05:28 +0200

    better result counting

--- src/main/scala/solver/evaluation/SolverEvaluator.scala
+++ src/main/scala/solver/evaluation/SolverEvaluator.scala
@@ -2,9 +2,16 @@ package solver
 package evaluation
 
 trait SolverEvaluator {
-  def benchmark[T](op: =>T) = {
+  def evaluate(solver: Solver)
+}
+
+case class Benchmarked[A](value: A, duration: Long) {
+  def map[B](op: A => B) = Benchmarked(op(value), duration)
+}
+
+object Benchmarked {
+  def apply[A](op: => A): Benchmarked[A] = {
     val start = System.currentTimeMillis()
-    (op, System.currentTimeMillis()-start)
+    new Benchmarked(op, System.currentTimeMillis() - start)
   }
-  def evaluate(solver : Solver)
 }
--- src/main/scala/solver/evaluation/Test.scala
+++ src/main/scala/solver/evaluation/Test.scala
@@ -3,6 +3,7 @@ package solver.evaluation
 import java.io.File
 import datacollection.TrainingProblemStore
 import solver.solvers.BruteForceSizeFilteredSolver
+import datacollection.BotApp
 
 object Test extends App {
   val evaluator = new CountCorrectInputsEvaluator(new TrainingProblemStore(new File("problems/train3")).allProblems.take(10))
