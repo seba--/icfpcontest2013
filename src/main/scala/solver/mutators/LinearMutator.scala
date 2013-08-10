01c6bb017db5bf453e4ab3eea9cf96ecc7a754ee
Dennis Albrecht <Dennis.Albrecht.tud@gmx.de>
2013-08-10 14:23:14 +0200

    LinearMutator merged

--- src/main/scala/solver/mutators/LinearMutator.scala
+++ src/main/scala/solver/mutators/LinearMutator.scala
@@ -5,6 +5,7 @@ import scala.util.Either
 import scala.collection.mutable.DoubleLinkedList
 import solver.Mutator
 import solver.ProblemSpec
+import lang.Metadata._
 
 object LinearMutator extends Mutator {
   
@@ -14,69 +15,68 @@ object LinearMutator extends Mutator {
   
   def notifyNewData(data: Map[Long, Long]): Unit = {}
   
-  def mutate(e: Exp): Option[Exp] = null 
-//  {
-//    if (e.size == 0)
-//      Some(DoubleLinkedList(Zero))
-//    else
-//      // TODO check: if mutateAtPosition can fail, it should return Option[Exp]
-//      Some(mutateAtPosition(e, specs.operators, e.size - 1))
-//  }
-//  
-//  def mutateAtPosition(e : Exp, op: List[Node], pos: Int) : Exp = {
-//    val nextOp : Either[Node, String] = mutateNode(e(pos))
-//    nextOp match {
-//      // if there is no further possible node, alternate the previous one
-//      case Right(_) => mutateAtPosition(e, op, pos - 1)
-//      case Left(n) =>
-//        val args = numOfArgs(n)
-//        // generate the new operator and the needed arguments
-//        val newOp = DoubleLinkedList(n)
-//        for (_ <- 0 to args)
-//          newOp ++ DoubleLinkedList(Zero)
-//        // replace the old operator by the new one
-//        var el = e
-//        for (_ <- 1 to pos)
-//          el = e.next
-//        el.next = newOp
-//        newOp.prev = el
-//        // if the new Exp is acceptable regarding the length then return it
-//        if (e.size + 1 > specs.size) // TODO: Check the "Add 1" for the program's lambda-expression
-//          mutateAtPosition(e, op, pos)
-//        else
-//          e
-//    }
-//  }
-//  
-//  def mutateNode(n: Node) : Either[Node, String] = {
-//    n match {
-//      case Zero => Left(One)
-//      case One => Left(MainVar)
-//      case MainVar => Left(specs.operators(0))
-//      case others => val index = specs.operators.indexOf(n) + 1
-//        if (index == specs.operators.size) 
-//          Right("No more operators")
-//        else
-//          Left(specs.operators(index))
-//    }
-//  }
-//  
-//  def numOfArgs(n: Node) : Int = {
-//    n match {
-//      case Zero => 0
-//      case One => 0
-//      case MainVar => 0
-//      case IfZero => 3
-//      case Not => 1
-//      case Shl1 => 1
-//      case Shr1 => 1
-//      case Shr4 => 1
-//      case Shr16 => 1
-//      case And => 2
-//      case Or => 2
-//      case Xor => 2
-//      case Plus => 2
-//      // TODO: Add a handling for fold
-//    }
-//  }
+  def mutate(e: Exp): Option[Exp] = {
+    if (e == null || size(e) == 1)
+      Some(Zero())
+    else
+      mutateAtPosition(e, e.size - 1)
+  }
+  
+  def mutateAtPosition(e : Exp, pos: Int) : Option[Exp] = {
+    if (pos < 0)
+      return None
+    val mutatedOp : Option[Node] = mutateNode(e(pos))
+    mutatedOp match {
+      case None => mutateAtPosition(e, pos - 1)
+      case Some(n) =>
+        val args = numOfArgs(n)
+        // generate the new operator and the needed arguments
+        val newArgs = DoubleLinkedList[Node]() ++ ((0 to args) map (_ => Zero ))
+        // replace the old operator by the new one
+        var el = e
+        for (_ <- 0 to pos)
+          el = e.next
+        var numOfOldArgs = numOfArgs(el.elem)
+        var lastOldArg = el
+        while (numOfOldArgs > 0) {
+          lastOldArg = lastOldArg.next
+          numOfOldArgs -= 1
+          numOfOldArgs += numOfArgs(lastOldArg.elem)
+        }
+        el.elem = n
+        el.next = newArgs
+        newArgs.prev = el
+        val restList = lastOldArg.next
+        
+        Some(e)
+    }
+  }
+  
+  def mutateNode(n: Node) : Option[Node] = {
+    n match {
+      case Zero => Some(One)
+      case One => Some(MainVar)
+      case MainVar => Some(specs.operators(0))
+      case others =>
+        val index = specs.operators.indexOf(n) + 1
+        if (index == specs.operators.size) 
+          None
+        else
+          Some(specs.operators(index))
+    }
+  }
+  
+  def numOfArgs(n: Exp) : Int = {
+    n match {
+      case Zero() => 0
+      case One() => 0
+      case MainVar() => 0
+      case FoldNext() => 0
+      case FoldAcc() => 0
+      case IfZero(_, _, _) => 3
+      case UApp(_, _) => 1
+      case BApp(_, _, _) => 2
+      case Fold(_, _, _) => 3
+    }
+  }
 }
